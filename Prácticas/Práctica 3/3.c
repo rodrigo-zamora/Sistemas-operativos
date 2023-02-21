e534658c385dcfc3a6afd05661f142243b9e84f9
Rodrigo Zamora <is727254@iteso.mx>
2023-02-21 08:23:03 -0600

    Práctica 3

--- Prácticas/Práctica 3/3.c
+++ Prácticas/Práctica 3/3.c
@@ -7,21 +7,23 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sched.h>
 
-#define ITERACIONES 2000000000
+#define ITERACIONES 20000000000
 #define PI 4
-#define NUM_HILOS 4
+#define NUM_HILOS 4 // Número de procesos a crear
 
-double sumatoria = 0;
+double sumas_parciales[NUM_HILOS]; // Arreglo para guardar las sumas parciales de cada hilo
 
-void calcular_sumatoria(double *sumatoria_parcial, int hilo_id)
+void calcular_suma_parcial(void *arg)
 {
-    double num_elevado;
+    int id_hilo = *((int *)arg);
     int i;
-    double inicio = hilo_id * (double)ITERACIONES / NUM_HILOS;
-    double fin = (hilo_id + 1) * (double)ITERACIONES / NUM_HILOS;
+    double num_elevado;
+    double *sumatoria_parcial = &sumas_parciales[id_hilo]; // Puntero a la variable de sumatoria parcial del hilo
 
-    for (i = inicio; i < fin; i++)
+    // Calcular la suma parcial correspondiente al hilo
+    for (i = id_hilo; i < ITERACIONES; i += NUM_HILOS)
     {
         num_elevado = pow(-1, i);
         *sumatoria_parcial += (num_elevado / (2 * i + 1));
@@ -34,37 +36,49 @@ int main()
     long long stop_ts;
     int elapsed_time;
     struct timeval ts;
+    int i, j;
+    double sumatoria = 0;
+    double pi_aproximado;
 
     gettimeofday(&ts, NULL);
     start_ts = ts.tv_sec * 1000000 + ts.tv_usec; // Tiempo inicial
 
-    // Arreglo de enteros para almacenar los IDs de los hilos
-    int hilos[NUM_HILOS];
-    int i;
+    // Crear procesos hijos para calcular las sumas parciales
     for (i = 0; i < NUM_HILOS; i++)
     {
-        hilos[i] = clone((void *)calcular_sumatoria, (void *)&sumatoria, CLONE_VM, (void *)i);
+        int *arg = malloc(sizeof(*arg));
+        *arg = i;
+        int pid = clone(calcular_suma_parcial,  // Función que ejecutará el proceso hijo
+                        malloc(16384) + 16384,  // Pila para el proceso hijo
+                        CLONE_VM | CLONE_FILES, // Flags para crear el proceso hijo
+                        arg);                   // Argumento que pasamos a la función calcular_suma_parcial
+        if (pid == -1)
+        {
+            perror("Error al crear proceso hijo");
+            exit(EXIT_FAILURE);
+        }
     }
 
-    // Esperar a que terminen todos los hilos
+    // Esperar a que terminen todos los procesos hijos
     for (i = 0; i < NUM_HILOS; i++)
     {
+        wait(NULL);
+    }
 
-        if (hilos[i] == -1)
-        {
-            perror("Error al crear el hilo");
-            exit(1);
-        }
-        waitpid(hilos[i], NULL, 0);
+    // Sumar todas las sumas parciales
+    for (j = 0; j < NUM_HILOS; j++)
+    {
+        sumatoria += sumas_parciales[j];
     }
 
     gettimeofday(&ts, NULL);
     stop_ts = ts.tv_sec * 1000000 + ts.tv_usec; // Tiempo final
 
     printf("Resultado: %lf\n", sumatoria);
-    double pi_aproximado = sumatoria * PI;
+    pi_aproximado = sumatoria * PI;
     printf("Aproximación de pi: %lf\n", pi_aproximado);
     elapsed_time = (int)(stop_ts - start_ts);
     printf("Proceso %d, %d microsegundos\n", getpid(), elapsed_time);
+
     return 0;
 }
